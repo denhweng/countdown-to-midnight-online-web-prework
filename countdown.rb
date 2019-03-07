70834afe9e9875009b2f2ab9ac2290e9c39f320e
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-07 07:39:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,12 +8,3 @@ end
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  time == number
-  while time > 0 
-  puts "#{time} SECONDS!"
-  time -= 1 
-  sleep 1 
-end
-  puts "HAPPY NEW YEAR!"
-end
